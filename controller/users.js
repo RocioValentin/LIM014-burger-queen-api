e1bdccca9a59b8169a60b4140b87b779f212abf3
RocioValentin <rociovcl.10@gmail.com>
2021-06-10 11:29:57 -0500

    verify Token  admin and auth

--- controller/users.js
+++ controller/users.js
@@ -1,6 +1,19 @@
+const User = require('../models/user');
 // Aquí debe ir la lógica de crear al usuario y
 // dar acceso a la bs
 module.exports = {
   getUsers: (req, resp, next) => {
+  },
+  createUsers: (req, resp, next) => {
+    const { email, password, role } = req.body;
+
+    const newAdminUser = new User({
+      email,
+      password,
+      role,
+    });
+    newAdminUser.save();
+    next();
+    return resp.json({ msg: 'usuario añadido' });
   },
 };
