798ceed24c87bcd0d8e110f63120b4132cde163c
Briceyda <Brd0208@gmail.com>
2021-06-11 01:27:36 -0500

    create user

--- controller/users.js
+++ controller/users.js
@@ -1,19 +1,52 @@
 const User = require('../models/user');
+
 // Aquí debe ir la lógica de crear al usuario y
 // dar acceso a la bs
 module.exports = {
   getUsers: (req, resp, next) => {
   },
   createUsers: (req, resp, next) => {
-    const { email, password, role } = req.body;
-
-    const newAdminUser = new User({
-      email,
-      password,
-      role,
+    const user = new User({
+      email: req.body.email,
+      password: req.body.password,
+      roles: req.body.roles.admin,
     });
-    newAdminUser.save();
+
+    user.save((err, usr) => {
+      err && resp.status(500).send(err.message);
+
+      resp.status(200).json(usr);
+    });
     next();
-    return resp.json({ msg: 'usuario añadido' });
+
+    // const user = new User({
+    //   email: req.body.email,
+    //   password: req.body.password,
+    //   roles: req.body.roles.admin,
+    // });
+    // console.log(user);
+    // const saveUser = User.save(user);
+    // saveUser.then((usr) => {
+    //   if (usr) {
+    //     resp.status(200).json(usr);
+    //   }
+
+    // })
+    //   .catch((err) => next(err));
+
+    // try {
+    //   console.log('entro al try');
+    //   const newUser = new User(req.body);
+    //   const user = await newUser.save(newUser);
+    //   resp.status(200).json({
+    //     _id: user._id,
+    //     email: user.email,
+    //     password: user.password,
+    //     roles: user.roles,
+    //   });
+    // } catch (err) {
+    //   console.log('entro al catch');
+    //   next(err);
+    // }
   },
 };
--- middleware/auth.js
+++ middleware/auth.js
@@ -21,18 +21,20 @@ module.exports = (secret) => (req, resp, next) => {
       return next(403);
     }
 
-    const user = User.findOne({ email: decodedToken.email });
+    const user = User.findById(decodedToken.uid);
 
     user.then((doc) => {
+      console.log(doc);
       if (!doc) {
         console.log('usuario no encontrado');
         return next(400);
       }
       req.userAuth = decodedToken;
-      console.log(req);
-      return resp.status(200).json({
-        msg: 'usuario autenticado',
-      });
+      return next();
+      // console.log('Soy:', req);
+      // return resp.status(200).json({
+      //   msg: 'usuario autenticado',
+      // });
     }).catch((err) => console.log(err));
 
     // TODO: Verificar identidad del usuario usando `decodeToken.uid`
@@ -40,7 +42,7 @@ module.exports = (secret) => (req, resp, next) => {
     //  new UsersService(); const { userId } = decodedToken; const user
     // = await usersService.getUser({ userId }); if (user && user.email ===
     // decodedToken.userEmail && user.roles.admin === decodedToken.userRol.admin)
-    // { req.userDecoded = decodedToken; return next(); } return next(401); }); 
+    // { req.userDecoded = decodedToken; return next(); } return next(401); });
   });
 };
 
@@ -54,13 +56,8 @@ module.exports.isAuthenticated = (req) => {
 // module.exports.isAuthenticated = (req) =>
 // { if (req.userDecoded) { return true; } return false; };
 
-module.exports.isAdmin = (req) => {
-  if (req.userAuth.role.admin === true) {
-    return true;
-  }
-  return false;
-  // TODO: decidir por la informacion del request si la usuaria es admin
-};
+module.exports.isAdmin = (req) => (req.userAuth.roles.admin);
+
 // es lo que primero se avanza
 module.exports.requireAuth = (req, resp, next) => (
   (!module.exports.isAuthenticated(req))
